4933d055f47eff88e58724bf434b2d4f82764ba9
Jampa Thinlay <jampathinlay@Jampas-MacBook-Pro.local>
2019-12-12 13:29:31 -0800

    deleted testing files and added custom button classname

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -10,9 +10,7 @@ const CheckoutItem = ({cartItem, clearItem, addItem, removeItem }) => {
         <div className='image-container'>
             <img src={imageUrl} alt='item' />
         </div>
-
         <span className='name'> {name} </span>
-      
         <span className='quantity'>
             <div className='arrow' onClick={() => removeItem(cartItem)}>&#10094;</div>
                <span className='value'> {quantity}</span>
@@ -32,4 +30,5 @@ const mapDispatchToProps = dispatch => ({
     addItem: item => dispatch(addItem(item)),
     removeItem: item => dispatch(removeItem(item))
 })
+
 export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import {PersistGate} from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
-
-
 import {store, persistor } from './redux/store';
 
 ReactDOM.render(
@@ -16,6 +14,5 @@ ReactDOM.render(
                 <App />
             </PersistGate>   
         </BrowserRouter> 
-    </Provider>
-    , document.getElementById('root'));
+    </Provider>, document.getElementById('root'));
 
